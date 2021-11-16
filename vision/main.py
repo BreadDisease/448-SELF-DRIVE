01375e2fbef3f88c68d4229b0e3f541e3fa96a5a
Aaron Nguyen <nguyendm@miamioh.edu>
2021-11-16 16:53:04 -0500

    Fix main

--- vision/main.py
+++ vision/main.py
@@ -2,8 +2,10 @@ import cv2
 import time
 from robotvision import RobotVision
 
-frame_rate = 10
-prev = 0
+frame_rate = 60
+prev = time.time()
+startTime = time.time()
+total_time = 30
 
 robot = RobotVision()
 
@@ -13,6 +15,7 @@ img_array = []
 while True:
 
     time_elapsed = time.time() - prev
+    seconds = int(time.time() - startTime)
     res, image = cap.read()
 
     if time_elapsed > 1.0/frame_rate:
@@ -21,16 +24,20 @@ while True:
         # Do something with your image here.
         #process_image()
         # cv2.imshow(image)
-        try:
-            frame = robot.resizeImage(image)
-            cv2.imshow('original', frame)
-            img_array.append(frame)
-            # img = robot.perceive(image, detection=False)
-            # cv2.imshow('Frame1', img)
-        except:
-            print("An exception occurred")
-            break     
+        cv2.imshow('original', image)
+        img_array.append(image)
+        # try:
+        #     frame = robot.resizeImage(image)
+        #     cv2.imshow('original', frame)
+        #     img_array.append(frame)
+        #     # img = robot.perceive(image, detection=False)
+        #     # cv2.imshow('Frame1', img)
+        # except:
+        #     print("An exception occurred")
+        #     break     
 
+    if seconds > 30:
+        break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
@@ -42,7 +49,7 @@ cv2.destroyAllWindows()
 height, width, layers = img_array[0].shape
 vidsize = (width,height)
 
-out = cv2.VideoWriter('output_level1.mov',cv2.VideoWriter_fourcc(*'DIVX'), 15, vidsize)
+out = cv2.VideoWriter('output_vid.mov',cv2.VideoWriter_fourcc(*'DIVX'), 15, vidsize)
  
 for i in range(len(img_array)):
     out.write(img_array[i])   
