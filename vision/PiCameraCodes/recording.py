f5bd12598990f6f0ccb364941a96417525afc926
Aaron Nguyen <nguyendm@miamioh.edu>
2021-11-16 15:56:32 -0500

    Add recording.py

--- vision/PiCameraCodes/recording.py
+++ vision/PiCameraCodes/recording.py
@@ -4,3 +4,10 @@ from picamera import PiCamera
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 60
+
+
+camera.start_recording("testVid.mov")
+
+time.sleep(10)
+
+camera.stop_recording()
