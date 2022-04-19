7c306e2e3111c71ffd03113f0102ae6eb702cac5
Aaron Nguyen <43735502+aaron-nguyen@users.noreply.github.com>
2022-04-19 15:59:33 -0400

    Update main.py
    Send output to arduino

--- vision/main.py
+++ vision/main.py
@@ -1,27 +1,22 @@
 import cv2
 import time
+import serial
 from robotvision import RobotVision
 import matplotlib.pyplot as plt
-
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # change the pic to the gray one and give name as linepic for using later to draw line.
-
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    # make the pic to be smooth and east to draw
-    canny_img = cv2.Canny(blur, 50, 150)
-    return canny_img
 
 frame_rate = 60
 prev = time.time()
 startTime = time.time()
-total_time = 300
+total_time = 300 # 5 minutes
 
-robot = RobotVision(detection=False)
+robot = RobotVision(detection=False, debugMode=False)
+ser = serial.Serial("/dev/ttyS0", 115200) # Open serial port to Arduino
 
 cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('roadtest1.mp4')
 
+meanOut = 0
+counter = 0
 while True:
     time_elapsed = time.time() - prev
     seconds = int(time.time() - startTime)
@@ -30,15 +25,20 @@ while True:
     if time_elapsed > 1.0/frame_rate:
         prev = time.time()
 
-        frame = robot.resizeImage(image, 50)
-        cv2.imshow('original', frame)
+        #frame = robot.resizeImage(image, 20)
+        #cv2.imshow('original', frame)
 
-        # canny_pic = canny(frame)
-        # cv2.imshow('canny', canny_pic)
+        isValid, output, _ = robot.perceive(image)
+        #cv2.imshow('Frame1', img)
 
-        tmpt, img = robot.perceive(image, 50)
-        cv2.imshow('Frame1', img)
-        #cv2.imshow('Temp', tmpt) 
+        # if isValid == 1:
+        #     # Expected output
+        #     #meanOut = (meanOut * counter + output) / (counter + 1)
+        #     meanOut = meanOut + (output - meanOut) / (counter + 1)
+        #     counter = counter + 1
+
+        msg = str(isValid) + "," + "{:.2f}".format(output) + "\n"
+        ser.write(msg.encode())
 
     if seconds > total_time:
         break
