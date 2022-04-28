0dc447dca5aaed904e739b0cef6c433e592c17dc
Aaron Nguyen <43735502+aaron-nguyen@users.noreply.github.com>
2022-04-28 01:45:33 -0400

    Update main.py

--- vision/main.py
+++ vision/main.py
@@ -4,51 +4,46 @@ import serial
 from robotvision import RobotVision
 import matplotlib.pyplot as plt
 
-frame_rate = 60
+frame_rate = 2
 prev = time.time()
 startTime = time.time()
-total_time = 300 # 5 minutes
+total_time = 100000 # 5 minutes
 
 robot = RobotVision(detection=False, debugMode=False)
 ser = serial.Serial("/dev/ttyS0", 115200) # Open serial port to Arduino
 
 cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('roadtest1.mp4')
+#cap = cv2.VideoCapture('level_1.mov')
 
-#meanOut = 0
-#counter = 0
-while True:
-    time_elapsed = time.time() - prev
-    seconds = int(time.time() - startTime)
-    res, image = cap.read()
+try:
+    while True:
+        time_elapsed = time.time() - prev
+        seconds = int(time.time() - startTime)
+        res, image = cap.read()
 
-    if time_elapsed > 1.0/frame_rate:
-        prev = time.time()
+        if time_elapsed > 1.0/frame_rate:
+            prev = time.time()
 
-        #frame = robot.resizeImage(image, 20)
-        #cv2.imshow('original', frame)
+            #frame = robot.resizeImage(image, 50)
+            #cv2.imshow('original', image) # frame)
 
-        isValid, output, _ = robot.perceive(image)
-        #cv2.imshow('Frame1', img)
+            isValid, output, img = robot.perceive(image,50)
+            cv2.imshow('Frame1', img)
 
-        # if isValid == 1:
-        #     # Expected output
-        #     #meanOut = (meanOut * counter + output) / (counter + 1)
-        #     meanOut = meanOut + (output - meanOut) / (counter + 1)
-        #     counter = counter + 1
+            msg = str(isValid) + "," + "{:.2f}".format(output) + "\n"
+            #print(msg)
+            ser.write(msg.encode())
 
-        msg = str(isValid) + "," + "{:.2f}".format(output) + "\n"
-        ser.write(msg.encode())
-
-    if seconds > total_time:
-        break
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if seconds > total_time:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+except:
+    print("Input ends")
 
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
-
 
 
